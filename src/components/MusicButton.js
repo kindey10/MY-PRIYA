805c10ea9c24710ab1f5ca2005395ff008532bd0
kindey10 <kindey2004@gmail.com>
2026-06-03 16:20:57 +0530

    remove duplicate music button component

--- src/components/MusicButton.js
+++ src/components/MusicButton.js
@@ -29,32 +29,3 @@ const MusicButton = () => {
 
 export default MusicButton;
 
-
-import "./MusicButton.css";
-
-
-const MusicButton = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const audioRef = useRef(new Audio("/music/song.mp3")); // Make sure you have a song here
-
-
-  const toggleMusic = () => {
-    if (isPlaying) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setIsPlaying(!isPlaying);
-  };
-
-
-  return (
-    <button className="music-button" onClick={toggleMusic}>
-      {isPlaying ? <FaPause /> : <FaPlay />} {/* Play/Pause icon */}
-    </button>
-  );
-};
-
-
-export default MusicButton;
-
